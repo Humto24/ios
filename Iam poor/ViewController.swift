853edf72d55a9140fbb02da5bcc40cab7c2a09b1
Richard Pohner <pohner.richard1985@gmail.com>
2023-08-23 18:57:18 +0200

    Initial Commit

---
+++ Iam poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iam poor
+//
+//  Created by Richárd Pohner on 23.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
